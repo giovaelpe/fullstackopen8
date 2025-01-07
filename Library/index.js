e3658283cd2994ce8408fe1b5e4f0b0eb31708c1
giovaelpe <saturnowgep@hotmail.com>
2025-01-07 19:34:36 +0000

    8.3 and 8.4

--- Library/index.js
+++ Library/index.js
@@ -102,6 +102,7 @@ const typeDefs = `
     name : String!
     id: String!
     born: Int!
+    bookCount: Int
   }
   type Book {
     title: String!
@@ -113,7 +114,8 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks: [Book]
+    allBooks(author: String): [Book]
+    allAuthors: [Author]
   }
 `;
 
@@ -121,7 +123,19 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((b) => b.author === args.author);
+    },
+    allAuthors: () =>
+      authors.map((author) => {
+        return {
+          ...author,
+          bookCount: books.filter((b) => b.author === author.name).length,
+        };
+      }),
   },
 };
 
