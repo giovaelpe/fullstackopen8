b5e6c8ae059fe73930ce7a900cc7f12a2f3df9bb
giovaelpe <saturnowgep@hotmail.com>
2025-02-27 19:04:09 +0000

    8.15

--- Library/index.js
+++ Library/index.js
@@ -2,86 +2,6 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { v1: uuid } = require("uuid");
 
-// Borrar
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "Demons",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
-//Borrar
-
 const Authors = require("./models/Authors");
 const Books = require("./models/Books");
 const mongoose = require("mongoose");
@@ -151,17 +71,46 @@ const resolvers = {
         author: author._id,
         genres: args.genres,
       });
+      if (newBook.title.length < 2) {
+        throw new GraphQLError("Book's title is too short", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       await newBook.save();
       return newBook;
     },
     addAuthor: async (root, args) => {
+      const exists = Authors.findOne({ name: args.name });
+      if (exists) {
+        throw new GraphQLError("Author already exists", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       const newAuthor = new Authors({ ...args });
+      if (newAuthor.name.length < 4) {
+        throw new GraphQLError("Author's name is too short", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       await newAuthor.save();
       return newAuthor;
     },
     editAuthor: async (root, args) => {
       const { name, setBornTo } = args;
       const authorToUpdate = await Authors.findOne({ name: name });
+      if (!authorToUpdate) {
+        throw new GraphQLError("Author doesn't exists", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       authorToUpdate.born = setBornTo;
       await authorToUpdate.save();
       return authorToUpdate;
