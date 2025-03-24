1616828f66bd7ba12e92dbaa4fe59ae98c549077
giovaelpe <saturnowgep@hotmail.com>
2025-03-24 11:08:58 +0000

    8.25 ready with notification

--- Library/library-frontend/src/App.jsx
+++ Library/library-frontend/src/App.jsx
@@ -6,15 +6,23 @@ import Loginform from "./components/Loginform";
 import { useApolloClient, useSubscription } from "@apollo/client";
 import Recomended from "./components/Recomended";
 import { ALL_BOOKS, BOOK_ADDED } from "./services/queries";
+import { Alert } from "react-bootstrap";
 
 const App = () => {
   const [page, setPage] = useState("authors");
   const [token, setToken] = useState(null);
   const client = useApolloClient();
+  const [alertMessage, setAlertMessage] = useState(null);
+
+  const showAlertMessage = (text) => {
+    setAlertMessage(text);
+    setTimeout(() => setAlertMessage(null), 5000);
+  };
 
   useSubscription(BOOK_ADDED, {
     onData: ({ data }) => {
       const addedBook = data.data.bookAdded;
+      showAlertMessage("Libro agregado!");
       client.cache.updateQuery({ query: ALL_BOOKS }, ({ allBooks }) => {
         return {
           allBooks: allBooks.concat(addedBook),
@@ -39,6 +47,7 @@ const App = () => {
 
   return (
     <div>
+      {alertMessage && <Alert variant="success">{alertMessage}</Alert>}
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
